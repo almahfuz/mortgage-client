044ec92b56cf78b4e0e760ce1329868508add470
almahfuz <abujafaralmahfuz@gmail.com>
2023-09-27 16:01:38 +0100

    test commit page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RegistrationPage from "./assets/components/layout/RegistrationPage.jsx";
+import LogIn from "./assets/components/Layout/LogIn.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+   // errorElement:<ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <RegistrationPage />,
+      },
+      {
+        path: "/login",
+        element: <LogIn />,
+      }
+     
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+  <RouterProvider router={router}></RouterProvider>
+ 
+);
